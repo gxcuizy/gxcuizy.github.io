064187ceeb4cc801cf002ca978be401033eee807
gxcuizy <gxcuizy@163.com>
2018-10-09 12:44:27 +0800

    Site updated: 2018-10-09 12:44:27

--- js/src/clickfont.js
+++ js/src/clickfont.js
@@ -2,7 +2,7 @@
 var a_idx = 0;
 jQuery(document).ready(function($) {
    $("body").click(function(e) {
-       var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
+       var a = new Array("写1个BUG", "不改BUG", "写10个BUG", "打死不改BUG", "写100个BUG", "BUG万岁", "别拦我写BUG" ,"写1000个BUG", "一个BUG一块钱", "改两个BUG打折", "这个不是BUG", "写无数个BUG");
        var $i = $("<span/>").text(a[a_idx]);
        a_idx = (a_idx + 1) % a.length;
        var x = e.pageX,
